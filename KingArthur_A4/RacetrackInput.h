2c3e59dfd8754e94cc60b89ca57ea615115120d2
Robert Imbler <robert.imbler@gmail.com>
2018-10-08 16:31:56 -0600

    Racetrack input done sans objects
    Bezier surfaces and bezier curves can be loaded from file. Object inputs do not exist yet, so not included.

--- KingArthur_A4/RacetrackInput.h
+++ KingArthur_A4/RacetrackInput.h
@@ -12,24 +12,29 @@
 
 using namespace std;
 
-bool loadControlPoints(char* filename) {
+bool loadRaceTrack(     char* filename, 
+                        int* curveCount,                // number of bezier surfaces
+                        vector<glm::vec3>* surface,     // all surface points
+                        int* trackPoints,               // number of track control points
+                        vector<glm::vec3>* track,       // track points
+                        vector<glm::vec3>* objects,     // object locations
+                        char* objectType,               // object type
+                        int* numObjects)                // object count
+{
 	ifstream fin(filename);
 	if (fin.fail()) return false;
 
-	//Find number of points
-	int numpoints;
+    //####################
+    //  Surface Collection
+    //####################
+	//Find number of points for bezier surfaces
 	string temp;
 	getline(fin, temp);
 	stringstream s(temp);
-	s >> numpoints;
-
-	if (numpoints % 16 != 0) {
-		fprintf(stdout, "Error: invalid number of points\n");
-		return false;
-	}
-
-	//Populate point vector
-	for (int i = 0; i < numpoints; i++) {
+	s >> *curveCount;
+    // start error handling here
+	// Collect all bezier surface points
+	for (int i = 0; i <  ( *curveCount * 4 ); i++) {
 		int x, y, z;
 		string line;
 		getline(fin, line);
@@ -43,10 +48,54 @@ bool loadControlPoints(char* filename) {
 		getline(t, temp);
 		stringstream sz(temp);
 		sz >> z;
-
 		glm::vec3 point(x, y, z);
-		//controlPoints.push_back(point);
+		(*surface).push_back(point);
 	}
+    //####################
+    //  Racetrack input
+    //####################
+    //Find number of points for bezier curves
+	getline(fin, temp);
+	stringstream s(temp);
+	s >> *trackPoints;
+	// Collect all bezier surface points
+	for (int i = 0; i < ( *trackPoints ); i++) {
+		int x, y, z;
+		string line;
+		getline(fin, line);
+		stringstream t(line);
+		getline(t, temp, ',');
+		stringstream sx(temp);
+		sx >> x;
+		getline(t, temp, ',');
+		stringstream sy(temp);
+		sy >> y;
+		getline(t, temp);
+		stringstream sz(temp);
+		sz >> z;
+		glm::vec3 point(x, y, z);
+		(*track).push_back(point);
+	}
+    //####################
+    //  Object Collection
+    //####################
+    //Find number of points for Objects
+    getline(fin, temp);
+	stringstream s(temp);
+	s >> *numObjects;
+    // if no objects exit now
+    if ( *numObjects == 0 ) return true;
+
+    // else grab all objects
+    /**
+     * Order of data collection
+     * <object type> 
+     * <object x, y, z> 
+     * <object orientation x, y, z> 
+     * <object size>
+     */
+	
+    // FIXME later when we actually have objects and shitttttt
 
 	return true;
 }
